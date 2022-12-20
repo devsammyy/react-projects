f20c12196aa3f840d225ac406ecb277958b1da32
devsammyy <yinkaadebayo31@gmail.com>
2022-12-20 21:17:33 +0100

    updated

---
+++ 1/src/components/Character.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Character = (character) => {
+  return (
+    <div className="col-3">
+      <div className="card">
+        <img
+          src={character.image}
+          alt={character.name}
+          width="300"
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <h3 className="card-title">{character.name}</h3>
+          <p className="text-left ">{`Origin: ${
+            character.origin && character.origin.name
+          }`}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Character;
